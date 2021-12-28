35e1eab4cae1b53a7e7084244e95ff24e5c523e8
Aiden <marcin.madry@interia.com>
2021-12-28 01:19:27 +0100

    Day12

---
+++ Day12/Part2/main.cpp
@@ -0,0 +1,82 @@
+#include <cstdio>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <cctype>
+
+typedef std::vector<std::vector<bool>> board;
+
+constexpr size_t con_sz = 16;
+bool visited[con_sz];
+std::vector<std::string> unique;
+
+// Forward declaration.
+board slurp_input(const std::string&);
+board connections = slurp_input("../input.txt");
+
+size_t find(const std::string& name)
+{
+    for (size_t i = 0; i < unique.size(); ++i) {
+        if (unique[i] == name)
+            return i;
+    }
+
+    unique.emplace_back(name);
+    return unique.size() - 1;
+}
+
+board slurp_input(const std::string& filename)
+{
+    std::ifstream file(filename);
+
+    board slurped(con_sz, std::vector<bool>(con_sz, false));
+    std::string line;
+    
+    while(getline(file, line)) {
+        auto dash = line.find_first_of('-');
+        std::string name = line.substr(0, dash);
+        std::string connection = line.substr(dash + 1);
+
+        size_t index_a = find(name);
+        size_t index_b = find(connection);
+
+        slurped[index_a][index_b] = true;
+        slurped[index_b][index_a] = true;
+    }
+
+    file.close();
+    return slurped;
+}
+
+void traverse(size_t start, bool twice, int& counter)
+{
+    if (start == find("end")) {
+        counter++;
+        return;
+    }
+    
+    for (size_t i = 0; i < unique.size(); ++i) {
+        if (connections[start][i]) {
+            if (!islower(unique[i][0])) {
+                traverse(i, twice, counter);
+            } else if (!visited[i]) {
+                visited[i] = true;
+                traverse(i, twice, counter);
+                visited[i] = false;
+            } else if (!twice && i != find("start")) {
+                traverse(i, true, counter);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int counter = 0;
+    
+    visited[find("start")] = true;
+    traverse(find("start"), false, counter);
+
+    printf("%d\n", counter);
+    return 0;
+}
